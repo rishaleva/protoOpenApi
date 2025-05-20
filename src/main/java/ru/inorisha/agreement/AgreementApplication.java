64afef343b1cd210cb9b9539e63edafabf2b5a51
Irina Inozemtseva <rishaleva@yandex.ru>
2025-05-20 16:40:27 +0300

    openapi

---
+++ src/main/java/ru/inorisha/agreement/AgreementApplication.java
@@ -0,0 +1,13 @@
+package ru.inorisha.agreement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgreementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgreementApplication.class, args);
+    }
+
+}
